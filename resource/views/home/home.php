604037a5df4f20061e74073e6259cfd135f18f75
bemac01 <nwankwochidubem01@gmail.com>
2025-04-17 16:33:00 +0100

    update

--- resource/views/home/home.php
+++ resource/views/home/home.php
@@ -7,7 +7,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="assets/img/favicon.png">
   <title>
-    Soft UI Design 3 System by Creative Tim
+   VTU App
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800" rel="stylesheet" />

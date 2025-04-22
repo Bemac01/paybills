bc85b159201f5c1086874ee47c73f9fd7259ebf8
bemac01 <nwankwochidubem01@gmail.com>
2025-04-22 15:01:03 +0100

    update Ajax

---
+++ assets/js/core/core.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    console.log("Core Jquery loaded");
+});
--- resource/views/auth/layout/header.php
+++ resource/views/auth/layout/header.php
@@ -15,11 +15,8 @@
   <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-icons.css" rel="stylesheet" />
   <link href="https://demos.creative-tim.com/argon-dashboard-pro/assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- Font Awesome Icons -->
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <!-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> -->
   <!-- CSS Files -->
   <link id="pagestyle" href="assets/css/soft-design-system.css?v=1.1.0" rel="stylesheet" />
-  <!-- Nepcha Analytics (nepcha.com) -->
-  <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
-  <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
 <body class="sign-in-illustration">
